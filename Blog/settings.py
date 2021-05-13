f4dde36d6b2944ecb45f1cceddb28b6cb7e7406f
unknown <y1j1woo@naver.com>
2021-05-13 22:01:59 +0900

    e

--- Blog/settings.py
+++ Blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +76,25 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':  'd7m6d0atlrbfdj',  # DATABASE
+        'USER' : 'zznvdjjxqvorhm',
+        'PASSWORD' : 'deeb2d5c5d2062b624f3e272aa3394fe0c87e5e98dff1f137d745973df134e2f',
+        'HOST' : 'ec2-18-215-111-67.compute-1.amazonaws.com',
+        'PORT' : '5432',  
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
